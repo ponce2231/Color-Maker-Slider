812993e09027b65433447d9bdf21d6c293ad9d3e
Christopher Ponce Mendez <christopherponcedev@gmail.com>
2019-05-30 22:24:06 -0400

    made the iboutlet and ibaction connections and added action for changing color with sliders

--- Color Maker Slider/ViewController.swift
+++ Color Maker Slider/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func changeColorComponent() {
+        
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
 }
 
